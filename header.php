b96d008777a83370cbe53b477e6034680239510e
Magnetar Man <me@magnetarman.com>
2018-07-03 05:02:46 +0200

    V. 0.0.3

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+<html lang="en">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Blog Template for Bootstrap</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preload" href="<?php echo get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
+
+      <script async src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script async src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <?php wp_head(); ?>
+      </head>
--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Blog Template for Bootstrap</title>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?php echo get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
+  <?php get_header(); ?>
 
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  </head>
 
   <body>
 
@@ -36,7 +23,7 @@
         <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
       </div>
 
-      <div class="row">
+      <div class="body">
 
         <div class="col-sm-8 blog-main">
 
@@ -140,16 +127,6 @@
           </div>
         </div><!-- /.blog-sidebar -->
 
-      </div><!-- /.row -->
+      </div><!-- /.body -->
 
-    </div><!-- /.container -->
-
-    <footer class="blog-footer">
-      <p>coded By MagnetarMan</p>
-        <a href="#">Back to top</a>
-      </p>
-    </footer>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-  </body>
-</html>
+<?php get_footer(); ?>
